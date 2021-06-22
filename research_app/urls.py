a8fdd20422475a4344a0cd60af8da4cd0c2a3e4c
rabbilyasar <rabbilyasar@gmail.com>
2021-06-23 00:21:29 +0600

    update api url
    Signed-off-by: rabbilyasar <rabbilyasar@gmail.com>

--- research_app/urls.py
+++ research_app/urls.py
@@ -20,5 +20,5 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='api/task/')),
-    path('api/task/', include('task.api')),
+    path('api/', include('task.api')),
 ]
