118db2582db1ac1238e824de6059c0649c291ac5
rabbilyasar <rabbilyasar@gmail.com>
2021-06-23 00:21:43 +0600

    add url for api
    Signed-off-by: rabbilyasar <rabbilyasar@gmail.com>

--- task/api.py
+++ task/api.py
@@ -1,9 +1,12 @@
 from task.models import Task
 from django.urls import path
-from .views import TaskListCreateAPIView, TaskRetrieveUpdateDestroyAPIView
+from .views import TaskListCreateAPIView, TaskRetrieveUpdateDestroyAPIView, TileListCreateAPIView, TileRetrieveUpdateDestroyAPIView
 
+app_name = 'task'
 
 urlpatterns = [
-    path('', TaskListCreateAPIView.as_view(), name='task-create-list'),
-    path('<str:pk>', TaskRetrieveUpdateDestroyAPIView.as_view(), name='task-update-delete'),
+    path('task', TaskListCreateAPIView.as_view(), name='task-create-list'),
+    path('task/<str:pk>', TaskRetrieveUpdateDestroyAPIView.as_view(), name='task-update-delete'),
+    path('tile', TileListCreateAPIView.as_view(), name='tile-create-list'),
+    path('tile/<str:pk>', TileRetrieveUpdateDestroyAPIView.as_view(), name='tile-update-delete'),
 ]
